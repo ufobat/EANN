84b8e7d4928bc50415d8bc73bea5ba2b90b1e72d
Martin Barth <martin@senfdax.de>
2017-03-17 16:27:33 +0100

    wip tile checks

--- src/main/java/org/eann/sim/simulation/World.java
+++ src/main/java/org/eann/sim/simulation/World.java
@@ -1,25 +1,28 @@
 package org.eann.sim.simulation;
 
-
 /**
  * Created by martin on 16.03.17.
  */
 public class World {
-    private Tile[][] tile;
+    private Tile[][] tiles;
 
     public World(int width, int length) {
-        this.tile = new Tile[width][length];
+        this.tiles = new Tile[width][length];
     }
 
     public Tile getTileAt(int x, int y) {
-        return this.tile[x][y];
+        return this.tiles[x][y];
     }
 
     public void setTitleAt(int x, int y, Tile tile) {
-        this.tile[x][y] = tile;
+        this.tiles[x][y] = tile;
     }
 
     public void addTitle(Tile newTile) {
         this.setTitleAt(newTile.getX(), newTile.getY(), newTile);
+    }
+
+    public Tile[][] getTiles() {
+        return this.tiles;
     }
 }
--- src/test/java/org/eann/sim/simulation/WorldFactoryTest.java
+++ src/test/java/org/eann/sim/simulation/WorldFactoryTest.java
@@ -3,6 +3,11 @@ package org.eann.sim.simulation;
 import org.eann.sim.configuration.Config;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by martin on 17.03.17.
  */
@@ -13,6 +18,14 @@ public class WorldFactoryTest {
         Config config = new Config();
         WorldFactory worldFactory = new WorldFactory(config.getWorld());
         World world = worldFactory.buildWorld();
-
+        Tile[][] tiles = world.getTiles();
+        for(int x = 0; x < tiles.length - 1; x++) {
+            for(int y = 0; y < tiles[x].length - 1; y++) {
+                Tile t = tiles[x][y];
+                assertNotNull("tile at (" + x + "," + y + ") is defined", t);
+                assertTrue("height on tile is <= 100", t.getHeight() <= 100f);
+                assertTrue("height on tile is >= -100", t.getHeight() >= -100f);
+            }
+        }
     }
 }
